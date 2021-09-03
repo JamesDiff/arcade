b31bf218c649b73e5da0811c81c36b1652a324c7
Your Name <your_email@real_email.edu>
2021-09-03 08:35:14 -0600

    Trying to push

--- app.js
+++ app.js
@@ -1,27 +1,21 @@
-/* In all of the games below you must figure out:
+//let initialState;
 
-What is the starting state?
-How do I display the state of the game to the user?
-What controls/interface to I make available to the user?
-How does each interaction update the state?
-In many ways, this is the "game loop": show state -> wait for input -> update state -> go to step 1 */
-
-// state
-let initialState;
-const statusDisplay = document.getElementById('#gameStatus'); //do i need # for id?
 const gameState = {
   players: ['x', 'o'], 
-  board: [  
+  board: [  //Boxes? empty strings?
     [null, null, null],
     [null, null, null],
     [null, null, null]
   ]
 }
-let currentPlayer = gameState.players['x']
+//determine which cell wasw clicked on the front end; it will go through a helper function that will help you determine whihc
+//cell in the virtual date (gamedstate.board)
 const boxes = Array.from(document.querySelectorAll('#gameGrid'));
-const playAgainButton = document.getElementById('#playAgain')
+const playAgainButton = document.getElementById('playAgain');
+const statusDisplay = document.getElementById('gameStatus'); 
+const boxDisplay = document.getElementsByClassName('box'); //made this element
 
-
+const currentPlayer = gameState.players['x'];
 /*const winningArrays = [    
   [0, 1, 2],
   [3, 4, 5],
@@ -34,43 +28,51 @@ const playAgainButton = document.getElementById('#playAgain')
 ] ; */
 
 
-
 function buildInitialState(currentPlayer) {
-  let board = gameState[board]
-  let currentPlayer;
+  //let board = gameState['board']; got rid of this
+  //let currentPlayer;
+  
+  boxDisplay.innterText = "null"; //added this
+  statusDisplay.innerText = "Choose a box";
+  renderState(boxes);
 }
 
 buildInitialState()
 
 
-function renderState(boxes){
-  for(let i = 0; i < boxes.length; i++){
-    boxes[i].innerText = ('x' || 'o')
-  }
-}
-  
-boxes.addEventListener('click', onBoardClick)
 
-function onBoardClick(){
-  let targetedBox = boxes[i]; 
+function renderState(boxes){
+  //for(let i = 0; i < boxes.length; i++){ 
+  //boxes[i].innerText = (null); 
+  //}
+}
+
+for(let i=0; i < boxes.length; i++){
+boxes[i].addEventListener('click', onBoardClick)
+}  
+
+
+function onBoardClick(event){
+  let targetedBox = event.target; 
+  //console.log(targetedBox)
   let playerSelection = currentPlayer
-  if(playerSelection = true && targetedBox === null){
-    targetedBox.innerText = playerSelection;
+  if(playerSelection = true && targetedBox == null){
+    boxDisplay.innerHTML = playerSelection;
   } else {
     statusDisplay.innerHTML = "Pick another box"; 
   }
-  renderState()
-}  
+  renderState();
+  //checkResults();
+  switchTurns();
 
-document.getElementById('gameGrid').addEventListener('click', switchTurns)
+}  
+//document.getElementById('gameGrid').addEventListener('click', switchTurns)
 
 function switchTurns(currentPlayer){ 
-  if(currentPlayer = 'x'){
-    let currentPlayer = 'o';
+  if(currentPlayer = gameState.players['x']){
+    let currentPlayer = gameState.players['o'];
   } else {
-    if(currentPlayer = !'x'){
-      let currentPlayer = x;
-    }
+    let currentPlayer = gameState.players['x'];
   }
   return currentPlayer
 }
@@ -91,61 +93,62 @@ function switchTurns(currentPlayer){
 
 
 
-function checkResults(){
+function checkResults(currentPlayer){ //what passes in here? //else statements necessary
+  if(boxes[0] === currentPlayer && boxes[1] === currentPlayer && boxes[2] === currentPlayer) {
+    statusDisplay.innerText = `${currentPlayer} wins`;
+      }
+  if(boxes[0] === currentPlayer && boxes[3] === currentPlayer && boxes[6] === currentPlayer) {
+    statusDisplay.innerText = `${currentPlayer} wins`;
+      }
+  if(boxes[0] === currentPlayer && boxes[4] === currentPlayer && boxes[8] === currentPlayer) {
+    statusDisplay.innerText = `${currentPlayer} wins`;
+      }
+  if(boxes[2] === currentPlayer && boxes[5] === currentPlayer && boxes[8] === currentPlayer) {
+    statusDisplay.innerText = `${currentPlayer} wins`;
+      }
+  if (boxes[6] === currentPlayer && boxes[7] === currentPlayer && boxes[8] === currentPlayer) {
+    statusDisplay.innerText = `${currentPlayer} wins`;
+      }
+  if (boxes[1] === currentPlayer && boxes[4] === currentPlayer && boxes[7] === currentPlayer) {
+    statusDisplay.innerText = `${currentPlayer} wins`;
+      }
+  if (boxes[3] === currentPlayer && boxes[4]=== currentPlayer && boxes[5] === currentPlayer) {
+    statusDisplay.innerText = `${currentPlayer} wins`;
+      }
+  if (boxes[2] === currentPlayer && boxes[4] === currentPlayer && boxes[6] === currentPlayer) {
+    statusDisplay.innerText = `${currentPlayer} wins`;
+      }
 
-    if(boxes[0] === currentPlayer && boxes[1] === currentPlayer && boxes[2] === currentPlayer) {
-      strategy.innerText = `${currentPlayer} wins`;
-      }
-    if(boxes[0] === currentPlayer && boxes[3] === currentPlayer && boxes[6] === currentPlayer) {
-      strategy.innerText = `${currentPlayer} wins`;
-      }
-    if(boxes[0] === currentPlayer && boxes[4] === currentPlayer && boxes[8] === currentPlayer) {
-      strategy.innerText = `${currentPlayer} wins`;
-      }
-    if(boxes[2] === currentPlayer && boxes[5] === currentPlayer && boxes[8] === currentPlayer) {
-      strategy.innerText = `${currentPlayer} wins`;
-      }
-    if (boxes[6] === currentPlayer && boxes[7] === currentPlayer && boxes[8] === currentPlayer) {
-      strategy.innerText = `${currentPlayer} wins`;
-      }
-    if (boxes[1] === currentPlayer && boxes[4] === currentPlayer && spaces[7] === currentPlayer) {
-      strategy.innerText = `${currentPlayer} wins`;
-      }
-    if (boxes[3] === currentPlayer && boxes[4]=== currentPlayer && boxes[5] === currentPlayer) {
-        strategy.innerText = `${currentPlayer} wins`;
-      }
-    if (boxes[2] === currentPlayer && boxes[4] === currentPlayer && boxes[6] === currentPlayer) {
-        strategy.innerText = `${currentPlayer} wins`;
-      }
+        //something here that closes the game/or resets the board  maybe then?
 
 }
 
 playAgainButton.addEventListener('click', playAgain)
 
 function playAgain(){
-  let currentPlayer = player1;
-  for(let value of board){
-    value = null;
-  }
-  for(let value of boxes){
-    value = null;
-  }
+  buildInitialState();
 }
-    
-singlePlayer.addEventListener('click', buttonClicked)
 
-function onePlayer(){  
+    
+singlePlayer.addEventListener('click', onePlayer())
+
+function onePlayer(){ 
+  let player1 = gameState.players['x'];
+  let computer = gameState.players['o'];
   let currentPlayer = player1;
-  let player1 = 'x'
-  let computer = 'o';
+
+  //let board/boxes ??
+  //somehow call onBoardClick
+
+
+
   const cornerBoxes = [boxes[0], boxes[2], boxes[6], boxes[8]];
   
-  let playerSelection = []; //not sure how to get their answers
+  let playerSelection = []; //not sure how to get number of player selection
  
-  for(i = 0; i < playerSelection.length; i++){}
-
-  if(playerSelection < 1){  /
-    if(boxes[0] === player1 || boxes[2] === player1) || boxes[6] === player1 || boxes[8] === player1){
+  for(i = 0; i < playerSelection.length; i++){
+    if(playerSelection < 1){  
+    if(boxes[0] === player1 || boxes[2] === player1 || boxes[6] === player1 || boxes[8] === player1){
       boxes[4].push(computer);
     }
     if (boxes[4] === currentPlayer) {
@@ -160,3 +163,6 @@ function onePlayer(){
     }
   }
 };
+}
+
+  
